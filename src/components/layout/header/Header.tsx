081016c067a6901ad87af86550457903aae23f37
Wing <48620014+Wilfreno@users.noreply.github.com>
2023-08-04 17:51:17 +0000

    improved navigation and header style

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,10 +5,17 @@ import SearchFilter from "./searchFilter/searchFilter";
 import Logo from "./Logo";
 import Notification from "./Notification";
 import UserLogin from "./UserLogin";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <section className={styles.container}>
+    <section
+      className={`${styles.container} ${
+        pathname.startsWith("/map") ? styles.map : styles.default
+      }`}
+    >
       <Logo />
       <SearchFilter />
       <div className={styles.icons}>
