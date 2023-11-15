fc722d560d92bba5d2971baaf4f405e3b7c3c7b2
Wing-09 <143404160+Wing-09@users.noreply.github.com>
2023-11-15 18:05:59 +0000

    redesigned main & map page

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -6,11 +6,14 @@ import Search from "./search/Search";
 import Menu from "./menu/Menu";
 
 export default function Header() {
+  const path_name = usePathname();
   return (
     <header
-      className={`absolute w-screen top-3 z-10 flex items-center justify-center bg-transparent sm:shadow-md sm:bg-white sm:top-0 sm:w-screen sm:my-0 sm:mb-10 sm:rounded-none md:mx-0`}
+      className={`${
+        path_name === "/" ? "sticky" : "absolute"
+      } w-screen my-5 z-10 flex items-center py-2 justify-center bg-transparent sm:shadow-md sm:bg-white sm:top-0 sm:w-screen sm:my-0 sm:mb-10 sm:rounded-none md:mx-0`}
     >
-      <div className="flex items-center justify-center border-2 rounded-full px-3 py-1 bg-white drop-shadow-lg sm:border-none sm:drop-shadow-none sm:w-full md:justify-evenly lg:justify-between sm:px-10 ">
+      <div className="flex items-center justify-center border-2 rounded-full px-3 py-1 bg-white drop-shadow-lg sm:border-none sm:drop-shadow-none sm:w-full sm:justify-evenly lg:justify-between sm:px-10 ">
         <Logo />
         <Search />
         <div
